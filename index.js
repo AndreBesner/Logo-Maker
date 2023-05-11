93f7c527378b1b8536ca7a5d5bfa29e2d89d99dc
Andre Besner <besner.andre@gmail.com>
2023-05-11 17:06:04 -0400

    add mock svg file data to makeSVG function to be coded around

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // include required packages
-
 const inquirer = require('inquirer');
 const fs = require('fs');
 // const svg = require('svg'); 
@@ -40,15 +39,29 @@ function userPrompts() {
     })
 }
 
+//invokes the user question prompts with inquirer
 userPrompts();
 
 
 
 // this function generates the svg file
 // takes answers from inquirer and dumps them into predefined text
-function generateSVG(data){
+// function generateSVG(data) {
+//   <svg
+//     version="1.1"
+//     width="300"
+//     height="200"
+//     xmlns="http://www.w3.org/2000/svg"
+//   >
+//     <rect width="100%" height="100%" fill="red" />
 
-}
+//     <circle cx="150" cy="100" r="80" fill="green" />
+
+//     <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">
+//       SVG
+//     </text>
+//   </svg>;
+// }
 
 
 // this function actaully writes the generated svg file to directory
